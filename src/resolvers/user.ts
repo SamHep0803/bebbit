c684c3f97a708683df4ab6b4b080cbceb0cb0464
Samuel Hepworth <samhep0803@gmail.com>
2021-08-16 15:19:28 +0400

    setup a lot of shit

---
+++ src/resolvers/user.ts
@@ -0,0 +1,98 @@
+import {
+	Arg,
+	Ctx,
+	Field,
+	InputType,
+	Mutation,
+	ObjectType,
+	Resolver,
+} from "type-graphql";
+import { User } from "../entities/User";
+import { MyContext } from "../types";
+import argon2 from "argon2";
+
+@InputType()
+class UsernamePasswordInput {
+	@Field()
+	username: string;
+
+	@Field()
+	password: string;
+}
+
+@ObjectType()
+class FieldError {
+	@Field()
+	field: string;
+
+	@Field()
+	message: string;
+}
+
+@ObjectType()
+class UserResponse {
+	@Field(() => [FieldError], { nullable: true })
+	errors?: FieldError[];
+
+	@Field(() => User, { nullable: true })
+	user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+	@Mutation(() => UserResponse)
+	async registerUser(
+		@Arg("options") options: UsernamePasswordInput,
+		@Ctx() { em }: MyContext
+	): Promise<UserResponse> {
+		if (options.username.length <= 2) {
+			return {
+				errors: [{ field: "username", message: "Username is too short" }],
+			};
+		}
+
+		if (options.password.length <= 3) {
+			return {
+				errors: [{ field: "password", message: "Password is too short" }],
+			};
+		}
+
+		const hashedPassword = await argon2.hash(options.password);
+		const user = em.create(User, {
+			username: options.username,
+			password: hashedPassword,
+		});
+		try {
+			await em.persistAndFlush(user);
+		} catch (error) {
+			if (error.code === "23505") {
+				return {
+					errors: [{ field: "username", message: "Username already exists" }],
+				};
+			}
+		}
+		return { user };
+	}
+
+	@Mutation(() => UserResponse)
+	async loginUser(
+		@Arg("options") options: UsernamePasswordInput,
+		@Ctx() { em }: MyContext
+	): Promise<UserResponse> {
+		const user = await em.findOne(User, {
+			username: options.username,
+		});
+		if (!user) {
+			return {
+				errors: [{ field: "username", message: "invalid username" }],
+			};
+		}
+		const valid = await argon2.verify(user.password, options.password);
+		if (!valid) {
+			return {
+				errors: [{ field: "password", message: "invalid password" }],
+			};
+		}
+		return { user };
+	}
+}
