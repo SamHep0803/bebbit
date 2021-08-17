631b5e3b9ff1c65f591f22875a23d9c65217f595
Samuel Hepworth <samhep0803@gmail.com>
2021-08-17 14:45:52 +0400

    rework folder structure and add client

---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
+import theme from "../theme";
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<ChakraProvider resetCSS theme={theme}>
+			<ColorModeProvider
+				options={{
+					useSystemColorMode: false,
+					initialColorMode: "dark",
+				}}
+			>
+				<Component {...pageProps} />
+			</ColorModeProvider>
+		</ChakraProvider>
+	);
+}
+
+export default MyApp;
